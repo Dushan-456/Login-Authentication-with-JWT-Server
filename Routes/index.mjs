13434b54febe89a8f068c9de14e77b4b54407dbb
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-08-02 23:56:05 +0530

    Delete index.mjs

--- Routes/index.mjs
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-// import userRouter from "./userRouter.mjs";
-
-const rootRouter = Router();
-
-// rootRouter.use("/user", userRouter);
-
-export default rootRouter;
