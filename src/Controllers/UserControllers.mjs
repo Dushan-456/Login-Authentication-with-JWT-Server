1812c6e879b1fe030ef1d126ef10e705b7f57d27
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-08-03 12:19:44 +0530

    Update UserControllers.mjs

--- src/Controllers/UserControllers.mjs
+++ src/Controllers/UserControllers.mjs
@@ -1,7 +1,8 @@
 import { matchedData, validationResult } from "express-validator";
 import { errorCreate } from "../Utils/error-creator.mjs";
 import Users from "../Models/Users.mjs";
-import  bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import { tokenGen } from "../Utils/jwt.mjs";
 
 class UserControllers {
    // Regiter  new user------------------------------------------------------------------------------------------------------------------------------
@@ -18,7 +19,7 @@ class UserControllers {
 
       const { username, email, mobile, role, con_password } = matchedData(req);
 
-      const hashedpwd = await bcrypt.hash(con_password,10);
+      const hashedpwd = await bcrypt.hash(con_password, 10);
 
       try {
          const newUser = await Users.create({
@@ -54,6 +55,69 @@ class UserControllers {
          return res.status(500).json({
             msg: "error",
             error: "Internal Server Error",
+         });
+      }
+   };
+
+   //  user login------------------------------------------------------------------------------------------------------------------------------
+
+   userLogin = async (req, res) => {
+      const error = validationResult(req);
+      const loginError = errorCreate(error.array());
+      if (error.array().length) {
+         return res.status(400).json({
+            msg: "error",
+            error: loginError,
+            data: null,
+         });
+      }
+
+      const { emailOrUsername, password } = matchedData(req);
+
+      try {
+         const user = await Users.findOne({
+            $or: [{ email: emailOrUsername }, { username: emailOrUsername }],
+         });
+
+         if (!user) {
+            return res.status(404).json({
+               msg: "error",
+               error: { general: "User not found" },
+               data: null,
+            });
+         }
+
+         const isMatch = await bcrypt.compare(password, user.password);
+         if (!isMatch) {
+            return res.status(401).json({
+               msg: "error",
+               error: { general: "Invalid credentials" },
+               data: null,
+            });
+         }
+         if (user && isMatch) {
+            // Token genarate with jwt auth---------------------------------------------------------------------------------------------
+            const payload = {
+               id: user._id,
+               username: user.username,
+               role: user.role,
+            };
+            const token = tokenGen(payload);
+
+            return res.status(200).json({
+               msg: "Login Successfull",
+               error: null,
+               data: {
+                  token,
+               },
+            });
+         }
+      } catch (error) {
+         console.error("Login Error:", err);
+         return res.status(500).json({
+            msg: "error",
+            error: { general: "Internal server error" },
+            data: null,
          });
       }
    };
