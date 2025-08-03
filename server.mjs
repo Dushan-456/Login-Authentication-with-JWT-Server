fe2a149619463f69412f36be6040074526ca7e04
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-08-03 21:34:12 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,12 +3,19 @@ import express from "express";
 import dbConnect from "./src/db/config.mjs";
 import cors from "cors";
 import rootRouter from "./src/Routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const server = express();
 const PORT = process.env.PORT || 5001;
 
-server.use(cors());
+server.use(
+   cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+   })
+);
 server.use(express.json());
+server.use(cookieParser())
 
 //ROUTE CONNECT
 server.use("/api/v1/", rootRouter);
